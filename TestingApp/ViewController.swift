516893229c0ee43be4c51c571cced6dd78e5aa34
Daniel Duan <daniel4duan@gmail.com>
2021-03-28 22:29:10 -0400

    Initial Commit

---
+++ TestingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestingApp
+//
+//  Created by Daniel Duan on 3/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestingAppTests/TestingAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  TestingAppTests.swift
+//  TestingAppTests
+//
+//  Created by Daniel Duan on 3/28/21.
+//
+
+import XCTest
+@testable import TestingApp
+
+class TestingAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
